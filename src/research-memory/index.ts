74e4b82cd1d9caafb7b29ebbd5ffad343a9ec739
ShihChi Huang <shh@theonlyperson.com>
2026-04-26 12:42:33 -0700

    feat(memory): add configurable postgres store

--- src/research-memory/index.ts
+++ src/research-memory/index.ts
@@ -1,4 +1,7 @@
+export * from "./factory.js";
 export * from "./lifecycle.js";
 export * from "./memory-store.js";
+export * from "./postgres-schema.js";
+export * from "./postgres-store.js";
 export * from "./schema.js";
 export * from "./store.js";
